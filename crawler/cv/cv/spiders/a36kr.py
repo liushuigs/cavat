36d922b36e2b2e4bc81a91b20b4f232eb9c83b4d
xijinling <jinlingx@everstring.com>
2016-04-16 02:08:33 -0700

    add parse logic to spiders

--- crawler/cv/cv/spiders/a36kr.py
+++ crawler/cv/cv/spiders/a36kr.py
@@ -1,6 +1,8 @@
 # -*- coding: utf-8 -*-
 import scrapy
-
+import datetime
+import json
+from cv.items.article import ArticleItem
 
 class A36krSpider(scrapy.Spider):
     name = "36kr"
@@ -10,6 +12,26 @@ class A36krSpider(scrapy.Spider):
     )
 
     def parse(self, response):
-        filename = 'data/'+response.url.split("/")[-1]
-        with open(filename, 'wb') as f:
-            f.write(response.body)
+        # filename = 'data/'+response.url.split("/")[-1]
+        # with open(filename, 'wb') as f:
+        #     f.write(response.body)
+        obj = response.css('.js-react-on-rails-component')\
+            .xpath('@data-props').extract()
+        result = json.loads(obj[0])
+        post = result['data']['post']
+
+        item = ArticleItem()
+        item['url'] = result['data']['router']
+        item['title'] = post['title']
+        item['content'] = post['display_content']
+        item['summary'] = post['summary']
+        item['published_ts'] = post['published_at']
+        item['created_ts'] = datetime.datetime.utcnow()
+        item['updated_ts'] = datetime.datetime.utcnow()
+        item['time_str'] = result['data']['router']
+        item['author_name'] = post['author']['display_name']
+        item['author_link'] = post['author']['domain_path']
+        item['author_avatar'] = post['author']['avatar']
+        item['tags'] = post['display_tag_list']
+        # print item
+        yield item
