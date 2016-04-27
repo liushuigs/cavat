2fb1cf3b49170b82a59a90a4c36ff022ee1b88ed
webeautiful <xiongfusong@gmail.com>
2016-04-28 07:05:27 +0800

    parse user info

--- crawler/cv/spiders/huxiu.py
+++ crawler/cv/spiders/huxiu.py
@@ -10,17 +10,22 @@ class HuxiuSpider(scrapy.Spider):
         #'http://www.huxiu.com/',
         'http://www.huxiu.com/article/146942',
     )
-    #custom_settings = {
-    #    'ITEM_PIPELINES': {
-    #        'cv.pipelines.a36kr.ArticlePipeline': 300
-    #    }
-    #}
+    custom_settings = {
+        'ITEM_PIPELINES': {
+            'cv.pipelines.a36kr.ArticlePipeline': 300
+        }
+    }
 
     def parse(self, response):
-        item = ArticleItem()
-        sel = response.selector
-
         print 'start'.center(100, '=')
+        item = self.parse_page(response)
+        print item['author_link']
+        print 'end'.center(100, '=')
+        yield item
+
+    def parse_page(self, response):
+        sel = response.selector
+        item = ArticleItem()
         item['url'] = response.url
         item['title'] = sel.xpath('//title/text()').extract()[0]
         item['content'] = sel.css('#article_content').extract()[0]
@@ -29,9 +34,9 @@ class HuxiuSpider(scrapy.Spider):
         item['created_ts'] = None
         item['updated_ts'] = None
         item['time_str'] = None
-        item['author_name'] = None
-        item['author_link'] = None
-        item['author_avatar'] = None
+        item['author_name'] = sel.css('.box-author-info').css('.author-name a::text').extract()[0]
+        item['author_link'] = sel.css('.box-author-info').css('.author-name a::attr(href)').extract()[0]
+        item['author_avatar'] = sel.css('.box-author-info').css('.author-face img::attr(src)').extract()[0]
         item['tags'] = None
         item['site_unique_id'] = None
         item['author_id'] = None
@@ -42,6 +47,4 @@ class HuxiuSpider(scrapy.Spider):
         item['source_type'] = None
         item['views_count'] = None
         item['cover'] = None
-        print item.get('url')
-        print 'end'.center(100, '=')
-        yield item
+        return item
