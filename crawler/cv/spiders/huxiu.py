7cb527601601425d2b43f7f4cf96f755905bdc6b
webeautiful <xiongfusong@gmail.com>
2016-04-28 23:12:23 +0800

    parse tags

--- crawler/cv/spiders/huxiu.py
+++ crawler/cv/spiders/huxiu.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 import scrapy
 from cv.items.article import ArticleItem
+import datetime
 
 
 class HuxiuSpider(scrapy.Spider):
@@ -10,34 +11,38 @@ class HuxiuSpider(scrapy.Spider):
         #'http://www.huxiu.com/',
         'http://www.huxiu.com/article/146942',
     )
-    custom_settings = {
-        'ITEM_PIPELINES': {
-            'cv.pipelines.a36kr.ArticlePipeline': 300
-        }
-    }
+    #custom_settings = {
+    #    'ITEM_PIPELINES': {
+    #        'cv.pipelines.a36kr.ArticlePipeline': 300
+    #    }
+    #}
 
     def parse(self, response):
         print 'start'.center(100, '=')
         item = self.parse_page(response)
-        print item['author_link']
+        print item['created_ts']
         print 'end'.center(100, '=')
         yield item
 
     def parse_page(self, response):
         sel = response.selector
         item = ArticleItem()
+
+        now_date = datetime.datetime.utcnow()
+        now_date = now_date.strftime('%Y-%m-%d %H:%M:%S')
+
         item['url'] = response.url
-        item['title'] = sel.xpath('//title/text()').extract()[0]
-        item['content'] = sel.css('#article_content').extract()[0]
+        item['title'] = sel.xpath('//title/text()').extract_first()
+        item['content'] = sel.css('#article_content').extract_first()
         item['summary'] = None
         item['published_ts'] = None
-        item['created_ts'] = None
-        item['updated_ts'] = None
+        item['created_ts'] = now_date
+        item['updated_ts'] = now_date
         item['time_str'] = None
-        item['author_name'] = sel.css('.box-author-info').css('.author-name a::text').extract()[0]
-        item['author_link'] = sel.css('.box-author-info').css('.author-name a::attr(href)').extract()[0]
-        item['author_avatar'] = sel.css('.box-author-info').css('.author-face img::attr(src)').extract()[0]
-        item['tags'] = None
+        item['author_name'] = sel.css('.box-author-info').css('.author-name a::text').extract_first()
+        item['author_link'] = sel.css('.box-author-info').css('.author-name a::attr(href)').extract_first()
+        item['author_avatar'] = sel.css('.box-author-info').css('.author-face img::attr(src)').extract_first()
+        item['tags'] = ','.join(sel.css('.tag-box').xpath(".//li[@class='transition']/text()").extract())
         item['site_unique_id'] = None
         item['author_id'] = None
         item['author_email'] = None
