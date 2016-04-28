118f46b820ef0dc2c6f7c3ddcfbc792f9d2511a3
webeautiful <xiongfusong@gmail.com>
2016-04-29 00:10:21 +0800

    complete a single page crawler

--- crawler/cv/spiders/huxiu.py
+++ crawler/cv/spiders/huxiu.py
@@ -2,6 +2,7 @@
 import scrapy
 from cv.items.article import ArticleItem
 import datetime
+from os.path import splitext, basename
 
 
 class HuxiuSpider(scrapy.Spider):
@@ -11,17 +12,14 @@ class HuxiuSpider(scrapy.Spider):
         #'http://www.huxiu.com/',
         'http://www.huxiu.com/article/146942',
     )
-    #custom_settings = {
-    #    'ITEM_PIPELINES': {
-    #        'cv.pipelines.a36kr.ArticlePipeline': 300
-    #    }
-    #}
+    custom_settings = {
+        'ITEM_PIPELINES': {
+            'cv.pipelines.article.ArticlePipeline': 300
+        }
+    }
 
     def parse(self, response):
-        print 'start'.center(100, '=')
         item = self.parse_page(response)
-        print item['created_ts']
-        print 'end'.center(100, '=')
         yield item
 
     def parse_page(self, response):
@@ -34,8 +32,8 @@ class HuxiuSpider(scrapy.Spider):
         item['url'] = response.url
         item['title'] = sel.xpath('//title/text()').extract_first()
         item['content'] = sel.css('#article_content').extract_first()
-        item['summary'] = None
-        item['published_ts'] = None
+        item['summary'] = sel.xpath('//meta[@name="description"]/@content').extract_first()
+        item['published_ts'] = self.datetime_str_to_utc(sel.css('.article-time::text').extract_first()+':00')
         item['created_ts'] = now_date
         item['updated_ts'] = now_date
         item['time_str'] = None
@@ -43,13 +41,20 @@ class HuxiuSpider(scrapy.Spider):
         item['author_link'] = sel.css('.box-author-info').css('.author-name a::attr(href)').extract_first()
         item['author_avatar'] = sel.css('.box-author-info').css('.author-face img::attr(src)').extract_first()
         item['tags'] = ','.join(sel.css('.tag-box').xpath(".//li[@class='transition']/text()").extract())
-        item['site_unique_id'] = None
-        item['author_id'] = None
+        item['site_unique_id'] = sel.css('.pl-report').xpath('@aid').extract_first()
+        item['author_id'] = splitext(basename(item['author_link']))[0]
         item['author_email'] = None
         item['author_phone'] = None
-        item['author_role'] = None
+        item['author_role'] = sel.css('.box-author-info').css('.icon-team-auth::attr(title)').extract_first()
         item['cover_real_url'] = None
         item['source_type'] = None
         item['views_count'] = None
         item['cover'] = None
         return item
+
+    @staticmethod
+    def datetime_str_to_utc(date_str):
+        timedelta = datetime.datetime.utcnow() - datetime.datetime.now()
+        local_datetime = datetime.datetime.strptime(date_str, "%Y-%m-%d %H:%M:%S")
+        result_utc_datetime = local_datetime - timedelta
+        return result_utc_datetime.strftime("%Y-%m-%d %H:%M:%S")
